5d4756754aef032f14b69f187f5648192e36fcb0
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2025-01-06 22:02:48 +0530

    Create circle and rectangle overlapping.java

---
+++ circle and rectangle overlapping.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int closestX=Math.max(x1,Math.min(xCenter,x2));
+        int closestY=Math.max(y1,Math.min(yCenter,y2));
+        int dx=closestX-xCenter;
+        int dy=closestY-yCenter;
+        int dist=dx*dx+dy*dy;
+        return dist<=radius*radius;
+       // Find the closest point on the rectangle to the circle's center
+      // Calculate the squared distance from the circle's center to the closest point
+       // Check if the distance squared is less than or equal to the squared radius
+
+    }
+}
